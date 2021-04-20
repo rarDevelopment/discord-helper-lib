cf72574dbb196a107bb378a58651a7fc3fab4509
Robert <4060573+rarDevelopment@users.noreply.github.com>
2021-04-20 00:33:06 -0400

    v1.2.0 - added image url stuff to sendErrorMessage and colour fields in MessageSender

--- MessageSender.js
+++ MessageSender.js
@@ -2,12 +2,23 @@ const MessageReplyDetails = require("./MessageReplyDetails");
 const MessageWithEmbed = require("./MessageWithEmbed");
 
 module.exports = class MessageSender {
+
+    RegularColor = 16777215;
+    ErrorColor = 16711680;
+
     sendMessage(messageToSend, channel) {
         return channel.createMessage(messageToSend);
     }
 
-    sendErrorMessage(errorMessage, argInput, username, channelToSend, replyMessageId) {      
-        const messageWithEmbed = new MessageWithEmbed(errorMessage, "Something went wrong!", null, `Requested by ${username}`, new MessageReplyDetails(replyMessageId, true));
+    sendErrorMessage(errorMessage, argInput, username, channelToSend, replyMessageId, imageUrl) {
+        const messageWithEmbed = new MessageWithEmbed(
+            errorMessage,
+            "Something went wrong!",
+            null,
+            `Requested by ${username}`,
+            new MessageReplyDetails(replyMessageId, true),
+            this.ErrorColor,
+            imageUrl);
         let messageToSend = messageWithEmbed.buildMessage();
         if (argInput) {
             messageToSend.embed.fields.push({
@@ -16,7 +27,7 @@ module.exports = class MessageSender {
                 inline: false
             });
         }
-        
+
         return this.sendMessage(messageToSend, channelToSend, null);
     }
 }
