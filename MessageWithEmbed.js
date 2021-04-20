3d25c1f37ffc1266fd71981163cea8bcb7c7d32e
Robert <4060573+rarDevelopment@users.noreply.github.com>
2021-04-20 00:14:43 -0400

    v1.1.0 - added image and colour to the embed options

--- MessageWithEmbed.js
+++ MessageWithEmbed.js
@@ -1,10 +1,12 @@
 module.exports = class MessageWithEmbed {
-    constructor(text, title, embedFields, footerText, messageReplyDetails) {
+    constructor(text, title, embedFields, footerText, messageReplyDetails, color, imageUrl) {
         this.text = text;
         this.title = title;
         this.embedFields = embedFields;
         this.footerText = footerText;
         this.messageReplyDetails = messageReplyDetails;
+        this.color = color;
+        this.imageUrl = imageUrl;
     }
 
     buildMessage() {
@@ -16,6 +18,12 @@ module.exports = class MessageWithEmbed {
         };
         if (this.text) {
             message.embed.description = this.text;
+        }
+        if (this.imageUrl) {
+            message.embed.image = { url: this.imageUrl }
+        }
+        if (this.color) {
+            message.embed.color = this.color;
         }
         if (this.embedFields) {
             message.embed.fields = this.embedFields.map(field => {
