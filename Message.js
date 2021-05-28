d7483ad8561d9b885180743fca50ce7ec0987214
Robert <4060573+rarDevelopment@users.noreply.github.com>
2021-05-27 21:44:45 -0400

    v1.5.0 - added Message (without embed) class

---
+++ Message.js
@@ -0,0 +1,23 @@
+module.exports = class MessageWithEmbed {
+    constructor(text, messageReplyDetails) {
+        this.text = text;
+        this.messageReplyDetails = messageReplyDetails;
+    }
+
+    buildMessage() {
+        let message = {
+            content: this.text
+        };
+        if (this.messageReplyDetails) {
+            message.messageReferenceID = this.messageReplyDetails.replyMessageId;
+            message.messageReference = { id: this.messageReplyDetails.replyMessageId }
+            message.allowedMentions = {
+                everyone: true,
+                roles: true,
+                users: true,
+                repliedUser: this.messageReplyDetails.mentionAuthor
+            };
+        }
+        return message;
+    }
+}
