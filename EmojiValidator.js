bbba63050ad3bd8c17b6a414dd18cd07f63152f6
Robert <4060573+rarDevelopment@users.noreply.github.com>
2021-09-26 12:40:51 -0400

    v1.6.0 - added emoji validator

---
+++ EmojiValidator.js
@@ -0,0 +1,33 @@
+module.exports = class EmojiValidator {
+    constructor(guildEmojis) {
+        this.guildEmojis = guildEmojis;
+    }
+    
+    getValidEmojiFromText(text) {
+        const regex = emojiRegex();
+        const emojiMatch = text.match(regex);
+        if (emojiMatch) {
+            const emoji = emojiMatch[0];
+            return emoji;
+        }
+        const customEmojiRegex = /(<a?)?:\w+:(\d{18}>)?/;
+        const customEmojiMatch = text.match(customEmojiRegex);
+        if (customEmojiMatch) {
+            const emoji = customEmojiMatch[0];
+            let guildHasEmoji = false;
+            this.guildEmojis.forEach(guildEmoji => {
+                const emojiNameToCheck = `${guildEmoji.name}:${guildEmoji.id}`;
+                if (emoji.toLowerCase().includes(emojiNameToCheck.toLowerCase())) {
+                    guildHasEmoji = true;
+                }
+            });
+            if (guildHasEmoji) {
+                return emoji;
+            }
+            else {
+                return null;
+            }
+        }
+        return null;
+    }
+}
