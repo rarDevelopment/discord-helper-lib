acfbabf31fe5c8d9d9cdfe4127d1a05adfb38659
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-02-13 14:22:31 -0500

    v1.7.0 - added MessageArrayJoiner

---
+++ MessageArrayJoiner.js
@@ -0,0 +1,24 @@
+module.exports = class MessageArrayJoiner {
+    buildMessageArrayFromStringArray(messagesToCombine, maxMessageLength, firstMessageTitle = '') {
+        let messageArray = [];
+        let arrayIndex = 0;
+
+        if (firstMessageTitle) {
+            messageArray.push(`${firstMessageTitle}\n`);
+        }
+
+        messagesToCombine.forEach(updateMessage => {
+            const messageToAdd = `${updateMessage}\n`;
+            const concatenatedMessage = messageArray[arrayIndex] + messageToAdd;
+            if (concatenatedMessage.length >= maxMessageLength) {
+                messageArray.push(messageToAdd);
+                arrayIndex += 1;
+            }
+            else {
+                messageArray[arrayIndex] += messageToAdd;
+            }
+        });
+
+        return messageArray;
+    }
+}
