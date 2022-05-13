8a4e57241f5374869a4db79a4ae72df3866e1969
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-05-12 20:35:20 -0400

    v1.11.0 - added titleUrl

--- MessageWithEmbed.js
+++ MessageWithEmbed.js
@@ -1,49 +1,53 @@
 module.exports = class MessageWithEmbed {
-    constructor(text, title, embedFields, footerText, messageReplyDetails, color, imageUrl) {
-        this.text = text;
-        this.title = title;
-        this.embedFields = embedFields;
-        this.footerText = footerText;
-        this.messageReplyDetails = messageReplyDetails;
-        this.color = color;
-        this.imageUrl = imageUrl;
-    }
+  constructor(text, title, embedFields, footerText, messageReplyDetails, color, imageUrl, titleUrl) {
+    this.text = text;
+    this.title = title;
+    this.embedFields = embedFields;
+    this.footerText = footerText;
+    this.messageReplyDetails = messageReplyDetails;
+    this.color = color;
+    this.imageUrl = imageUrl;
+    this.titleUrl = titleUrl;
+  }
 
-    buildMessage() {
-        let message = {
-            embed: {
-                title: this.title,
-                footer: { text: this.footerText }
-            }
-        };
-        if (this.text) {
-            message.embed.description = this.text;
-        }
-        if (this.imageUrl) {
-            message.embed.image = { url: this.imageUrl }
-        }
-        if (this.color) {
-            message.embed.color = this.color;
-        }
-        if (this.embedFields) {
-            message.embed.fields = this.embedFields.map(field => {
-                return {
-                    name: field.name,
-                    value: field.value,
-                    inline: field.inline
-                };
-            })
-        }
-        if (this.messageReplyDetails) {
-            message.messageReferenceID = this.messageReplyDetails.replyMessageId;
-            //message.messageReference = { id: this.messageReplyDetails.replyMessageId }
-            message.allowedMentions = {
-                everyone: true,
-                roles: true,
-                users: true,
-                repliedUser: this.messageReplyDetails.mentionAuthor
-            };
-        }
-        return message;
+  buildMessage() {
+    let message = {
+      embed: {
+        title: this.title,
+        footer: { text: this.footerText }
+      }
+    };
+    if (this.text) {
+      message.embed.description = this.text;
     }
+    if (this.imageUrl) {
+      message.embed.image = { url: this.imageUrl }
+    }
+    if (this.titleUrl) {
+      message.embed.url = this.titleUrl;
+    }
+    if (this.color) {
+      message.embed.color = this.color;
+    }
+    if (this.embedFields) {
+      message.embed.fields = this.embedFields.map(field => {
+        return {
+          name: field.name,
+          value: field.value,
+          inline: field.inline
+        };
+      })
+    }
+    if (this.messageReplyDetails) {
+      message.messageReferenceID = this.messageReplyDetails.replyMessageId;
+      //message.messageReference = { id: this.messageReplyDetails.replyMessageId }
+      message.allowedMentions = {
+        everyone: true,
+        roles: true,
+        users: true,
+        repliedUser: this.messageReplyDetails.mentionAuthor
+      };
+    }
+    return message;
+  }
 }
