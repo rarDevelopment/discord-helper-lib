258747f20bd98e93cb75a122718a71a3acb5ebb9
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-07-26 01:45:34 -0400

    v1.15.0 - fix EventRegistration, add subcommands to CommandRegistration

--- CommandRegistration.js
+++ CommandRegistration.js
@@ -2,12 +2,23 @@ module.exports = class CommandRegistration {
     registerCommands(bot, commands) {
         commands.forEach(command => {
             bot.registerCommand(command.name, command.execute.bind(command), {
-                description: command.help.usage,
-                fullDescription: command.help.message
+                description: command.help.message,
+                fullDescription: command.help.usage,
+                cooldown: command.cooldown,
+                caseInsensitive: true,
             });
             if (command.aliases) {
                 command.aliases.forEach(alias => {
                     bot.registerCommandAlias(alias, command.name);
+                });
+            }
+            if (command.subcommands) {
+                command.subcommands.forEach(subcommandName => {
+                    bot.commands[command.name].registerSubcommand(subcommandName, command[subcommandName].bind(command), {
+                        description: command.help.usage.replace(command.name, subcommandName),
+                        cooldown: command.cooldown,
+                        caseInsensitive: true
+                    });
                 });
             }
         });
--- EventRegistration.js
+++ EventRegistration.js
@@ -1,7 +1,7 @@
 module.exports = class EventRegistration {
     registerEvents(bot, events) {
         events.forEach(event => {
-            bot.on(event.name, event.execute);
+            bot.on(event.name, event.execute.bind(event));
         });
     }
 }
